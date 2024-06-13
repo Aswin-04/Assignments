962939a5253c0cc995e4426cddf42a3399d8dfcc
Aswin-04 <e.aswin004@gmail.com>
2024-06-13 19:28:18 +0530

    Finish calculator.js

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,42 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+
+  constructor() {
+    this.result = 0;
+  }
+
+  add(num) {
+    this.result+=num
+  }
+
+  subtract(num) {
+    this.result-=num
+  }
+
+  multiply(num) {
+    this.result*=num
+  }
+
+  divide(num) {
+    if(num === 0) throw new Error("Zero Division Error")
+    this.result/=num
+  }
+
+  clear() {
+    this.result = 0
+  }
+
+  getResult() {
+    return this.result
+  }
+
+  calculate(str) {
+    let resultStr = eval(str)
+    if(!isFinite(resultStr)) throw new Error("Zero Division Error")
+    this.result = resultStr
+  }
+}
 
 module.exports = Calculator;
