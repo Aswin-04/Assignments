99b77ab48429a9bf2db3ef26d0fdb77964d910dc
Aswin-04 <e.aswin004@gmail.com>
2024-06-13 11:19:47 +0530

    Finish anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,27 @@
 
 function isAnagram(str1, str2) {
 
+  if(str1.length != str2.length) return false
+  let len = str1.length
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+
+  const sortedStr1 = str1.split("").sort().join("")
+  const sortedStr2 = str2.split("").sort().join("")
+
+  return sortedStr1 == sortedStr2
+
+  // let arr = []
+  // for(let i=0; i < len; i++) {
+  //   for(let j=0; j < len; j++) {
+  //     if(str1[i] == str2[j]) {
+  //       arr.push(str1[i])
+  //       break;
+  //     }
+  //   }
+  // }
+  // let str3 = arr.join("")
+  // return str1 == str3 ? true : false
 }
 
 module.exports = isAnagram;
