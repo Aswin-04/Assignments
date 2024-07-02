1c91022cab36be5c4f7617f9a1f3893da7ba726a
Aswin-04 <e.aswin004@gmail.com>
2024-07-02 21:26:17 +0530

    Finish 04-mongo-with-jwt-auth

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,30 @@
+const jwt = require("jsonwebtoken")
+const {JWT_SECRET} = require("../config");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.Authentication
+    const words = token.split(" ")
+    const jwtToken = words[1]
+
+    const decodedValue = jwt.verify(jwtToken, JWT_SECRET)
+
+    try{
+        if(decodedValue.username){
+            req.username = decodedValue.username
+            next()
+        } else {
+            res.status(403).json({
+                msg: "You are not authenticated"
+            })
+        }
+    } catch(e) {
+        res.status(404).json({
+            msg: "Incorrect inputs"
+        })
+    }
 }
 
 module.exports = userMiddleware;
