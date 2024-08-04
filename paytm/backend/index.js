4fe085490e7bfbb5b6366217170f89a1a26e9fd9
Aswin-04 <e.aswin004@gmail.com>
2024-08-04 17:27:05 +0530

    completed paytm backend

--- paytm/backend/index.js
+++ paytm/backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const rootRouter = require('./routes/index')
+const cors = require('cors')
+const app = express();
 
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/v1',rootRouter)
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`)
+})
 
---
+++ paytm/backend/middlewares/userAuth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config')
+
+function authMiddleware(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+  if(!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(403).json({})
+  }
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if(err) return res.status(403).json({})
+
+    if(decoded.userId) {
+      req.userId = decoded.userId
+      next()
+      
+    } else {
+      return res.status(403).json({})
+    }
+  })
+}
+
+module.exports = {authMiddleware}
