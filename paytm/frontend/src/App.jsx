f63ea75e6c96ad61d3f8ca8dcee5b7bdf97ee10f
Aswin-04 <e.aswin004@gmail.com>
2024-09-16 18:41:45 +0530

    complete routing

--- paytm/frontend/src/App.jsx
+++ paytm/frontend/src/App.jsx
@@ -1,10 +1,41 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+import {
+  Dashboard,
+  Signin,
+  Signup,
+  SendMoney
+} from './components/'
+
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <Signup/>,
+  },
+  {
+    path: "/signin",
+    element: <Signin/>,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard/>,
+  },
+  {
+    path: "/send",
+    element: <SendMoney/>,
+  }
+])
+
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   )
 }
 
